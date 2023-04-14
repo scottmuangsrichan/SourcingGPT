c035afa34bc21015128d2cb4a9db466943c67836
Scott Muangsrichan <scott@modernphase.io>
2023-04-13 20:58:47 -0700

    fixed: added gpt model

--- src/pages/api/openAi/index.ts
+++ src/pages/api/openAi/index.ts
@@ -1,7 +1,14 @@
 import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
-    organization: "YOUR_ORG_ID",
+    organization: "org-XoCDKxXU5SQKlyZpLwwsMpI2",
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
+const response = openai.listEngines();
+
+const completion = await openai.createChatCompletion({
+    model: "gpt.3.5-turbo",
+    messages: [{role: "user" , content: "Hello World"}]
+});
+
+console.log(completion.data.choices[0].message);
